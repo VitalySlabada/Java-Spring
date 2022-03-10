0190b69dcb9c02d6a8479788cfd8b2d6b3aa8da7
VitalySlabada <vitaly.slabada@gmail.com>
2022-03-10 15:12:40 -0600

    Basic Form with web and thymeleaf

---
+++ Spring_Practice_Forms/src/test/java/com/example/spring_practice_forms/SpringPracticeFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_practice_forms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeFormsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
