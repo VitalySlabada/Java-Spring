6e33479ce020b6e374d73c94508ae41ebe5120c7
VitalySlabada <vitaly.slabada@gmail.com>
2022-05-03 20:01:07 -0500

    ORM modules

--- Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-13-ORM-CinemaLab/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,16 +1,19 @@
 package com.cydeo.bootstrap;
 
 import com.cydeo.entity.Account;
+import com.cydeo.enums.UserRole;
 import com.cydeo.repository.AccountRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
 
     private final AccountRepository accountRepository;
+
 
     public DataGenerator(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
@@ -18,7 +21,25 @@ public class DataGenerator implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("**********************   Account start **************");
+        System.out.println(accountRepository.findByCountryOrState("United States", "Alabama"));
+        System.out.println();
+        accountRepository.findByAgeLessThanEqual(35).forEach(System.out::println);
+        System.out.println();
+        accountRepository.findByRole(UserRole.USER).forEach(System.out::println);
+        System.out.println();
+        //accountRepository.streamByAgeBetween(30,40).forEach(System.out::println);
+        accountRepository.findByAgeBetween(20,40).forEach(System.out::println);
+        System.out.println();
+        accountRepository.findByAddressStartsWith("26").forEach(System.out::println);
+        accountRepository.findByOrderByAge().forEach(x -> System.out.println(x.getName()));
 
+        System.out.println("%$%$%$%$%$ JPQL %$%$%$%$%$%$%$");
+        accountRepository.getAllAdminAccounts().forEach(x-> System.out.println(x.getName()));
+        accountRepository.getAllAccountsSortedByAge().forEach(x-> System.out.println(x.getName()));
+        System.out.println("%$%$%$%$%$ NATIVE %$%$%$%$%$%$%$");
+
+        System.out.println("**********************   Account finish *************");
 
 
     }
